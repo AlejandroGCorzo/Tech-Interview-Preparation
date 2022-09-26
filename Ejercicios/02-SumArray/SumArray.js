33e7b9185da8f490de6365cf7aa86ed5df797a61
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-09-26 10:40:35 -0300

    SumArray more efficient

--- Ejercicios/02-SumArray/SumArray.js
+++ Ejercicios/02-SumArray/SumArray.js
@@ -1,9 +1,25 @@
 function SumArray(arr, n) {
   // Your code here:
-  for (let i = 0; i < arr.length; i++) {
-    if (arr.some((el) => el != arr[i] && el + arr[i] === n)) return true;
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (arr.some((el) => el != arr[i] && el + arr[i] === n)) return true;
+  // }
+  // return false;
+  arr.sort();
+  let i = 0;
+  let j = arr.length - 1;
+  while (i != j) {
+    const sum = arr[i] + arr[j];
+    if (sum === n) return true;
+    if (sum > n) j--;
+    else i++;
   }
   return false;
 }
+
+console.log(SumArray([2, 4, 5, 9], 9));
+console.log(SumArray([2, 4, 5, 9], 12));
+console.log(SumArray([2, 5, 9], 4));
+console.log(SumArray([4, 2, 9, 5], 7));
+console.log(SumArray([5, 2, 9, 4], 12));
 
 module.exports = SumArray;
