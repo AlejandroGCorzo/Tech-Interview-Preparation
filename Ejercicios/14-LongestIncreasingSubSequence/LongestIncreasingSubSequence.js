aeb1e23155b89e4d4bfcca9663dd4fd45d1c8a39
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-10-01 17:31:49 -0300

    Update LongestIncreasingSubSequence.js

--- Ejercicios/14-LongestIncreasingSubSequence/LongestIncreasingSubSequence.js
+++ Ejercicios/14-LongestIncreasingSubSequence/LongestIncreasingSubSequence.js
@@ -24,8 +24,8 @@ function longestIncreasingSubsequence(array, sequences = [], longest = 0) {
   // );
   return longest;
 }
-console.log(longestIncreasingSubsequence([1, 2, 3, 4]));
-// console.log(longestIncreasingSubsequence([3, 10, 4, 5])); //(3)
+// console.log(longestIncreasingSubsequence([1, 2, 3, 4]));
+console.log(longestIncreasingSubsequence([3, 10, 4, 5])); //(3)
 // console.log(longestIncreasingSubsequence([3, 4, 2, 1, 10, 6])); //(3)
 // console.log(longestIncreasingSubsequence([10, 22, 9, 33, 20, 50, 41, 60, 80])); //(6)
 // console.log(
@@ -33,5 +33,43 @@ console.log(longestIncreasingSubsequence([1, 2, 3, 4]));
 //     10, 22, 9, 33, 20, 50, 41, 60, 80, 21, 23, 24, 25, 26, 27, 28,
 //   ])
 // ); //(9)
+
+/*
+
+[
+  [3],
+  [10],
+  [3, 10],
+  [4],
+  [3, 4],
+  [5],
+  [3, 5],
+  [4, 5],
+  [3, 4, 5]
+]
+
+[
+  [3],
+  [4],
+  [3, 4],
+  [2],
+  [1],
+  [10],
+  [3, 10],
+  [4, 10],
+  [3, 4, 10],
+  [2, 10],
+  [1, 10],
+  [6],
+  [3, 6],
+  [4, 6],
+  [3, 4, 6],
+  [2, 6],
+  [1, 6],
+]
+
+
+*/
+
 
 module.exports = longestIncreasingSubsequence;
