073932ccff831be1dcf0714fe1d44021ee7b507a
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-10-01 15:48:31 -0300

    Update SumArray.js

--- Ejercicios/02-SumArray/SumArray.js
+++ Ejercicios/02-SumArray/SumArray.js
@@ -5,15 +5,25 @@ function SumArray(arr, n) {
   // }
   // return false;
   // arr.sort();
-  let i = 0;
-  let j = arr.length - 1;
-  while (i != j) {
-    const sum = arr[i] + arr[j];
-    if (sum === n) return true;
-    if (sum > n) j--;
-    else i++;
+
+  if (arr.length === 1) return false;
+  let aux = arr.shift();
+
+  for (const el of arr) {
+    if (aux + el === n) return true;
   }
+  if (SumArray(arr, n)) return true;
   return false;
+
+  // let i = 0;
+  // let j = arr.length - 1;
+  // while (i != j) {
+  //   const sum = arr[i] + arr[j];
+  //   if (sum === n) return true;
+  //   if (sum > n) j--;
+  //   else i++;
+  // }
+  // return false;
 }
 
 console.log(SumArray([2, 4, 5, 9], 9));
